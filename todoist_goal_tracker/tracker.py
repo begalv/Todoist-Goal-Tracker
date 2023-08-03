acdf6a723cc043c81a36adc9df95ddc7cd3d19ae
begalv <bcgalv@gmail.com>
2023-08-03 14:49:38 -0300

    setting up the enviroment

---
+++ todoist_goal_tracker/tracker.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import locale
+import numpy as np
+import pkg_resources
+
+
+#------------ FETCHING AND FILTERING THE DATA ------------
+
+def get_tasks_from_todois_csv():
+    pass
